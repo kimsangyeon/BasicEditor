e757f0bb80ff65801bde1bf341764511fb96ee77
Kim Sangyeon <dho113@naver.com>
2019-05-17 13:55:40 +0900

    init basic-editor

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './App.css';
+
+import logo from './logo.svg';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App"></div>
+    );
+  }
+}
+
+export default App;
