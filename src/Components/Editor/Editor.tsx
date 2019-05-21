04477d5685361e6d831deaf8ca6d6bb12920646e
Kim Sangyeon <dho113@naver.com>
2019-05-22 01:21:00 +0900

    :chart_with_upwards_trend: init BasicEditor & add onKeydown event

--- src/Components/Editor/Editor.tsx
+++ src/Components/Editor/Editor.tsx
@@ -1,11 +1,19 @@
 import * as React from 'react';
+import BasicEditor from '../../js/BasicEditor';
 
 class Editor extends React.Component {
-  public render() {
-    return (
-      <div id="basicEditor" className="Editor" contentEditable={true}/>
-    );
-  }
+    public editor: BasicEditor;
+    constructor(props: Readonly<{}>) {
+        super(props);
+        this.editor = new BasicEditor('basicEditor');
+    }
+
+    public render() {
+        const keyboradHandler = this.editor.getKeyboardHandler();
+        return (
+        <div id="basicEditor" className="Editor" contentEditable={true} onKeyDown={keyboradHandler.onKeydown}/>
+        );
+    }
 }
 
 export default Editor;
