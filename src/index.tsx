47acafd8f5345b402f785270931b6da9421d7f08
Kim Sangyeon <dho113@naver.com>
2019-05-23 18:24:31 +0900

    :meat_on_bone: modify indent index.tsx
    - remove Editor className

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import registerServiceWorker from './registerServiceWorker';
 
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
+    <App />,
+    document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
