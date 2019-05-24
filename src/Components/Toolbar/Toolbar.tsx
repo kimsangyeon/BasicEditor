61f4a017c78448ae8f1c7649534db889918d6175
Kim Sangyeon <dho113@naver.com>
2019-05-24 13:52:32 +0900

    :carousel_horse: create StrikeThrough.tsx
    - import StrikeThrough in Toolbar

--- src/Components/Toolbar/Toolbar.tsx
+++ src/Components/Toolbar/Toolbar.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
 import Bold from './Icons/Bold';
 import Italic from './Icons/Italic';
+import StrikeThrough from './Icons/StrikeThrough';
 import Underline from './Icons/Underline';
 
 class Toolbar extends React.Component {
     public render() {
         return (
             <div id="be-toolbar" className="Toolbar">
-                <Bold /><Italic/><Underline/>
+                <Bold /><Italic/><Underline/><StrikeThrough/>
             </div>
         );
     }
