e8654eff6a0c647c8529a66333a9725d58769073
Kim Sangyeon <dho113@naver.com>
2019-05-24 13:43:04 +0900

    :speedboat: create Underline.tsx
    - import Underline in Toolbar

--- src/Components/Toolbar/Toolbar.tsx
+++ src/Components/Toolbar/Toolbar.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import Bold from './Icons/Bold';
 import Italic from './Icons/Italic';
+import Underline from './Icons/Underline';
 
 class Toolbar extends React.Component {
     public render() {
         return (
             <div id="be-toolbar" className="Toolbar">
-                <Bold /><Italic/>
+                <Bold /><Italic/><Underline/>
             </div>
         );
     }
