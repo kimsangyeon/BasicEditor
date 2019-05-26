beab6defc45113909e99e9903da477f5e1ce061d
Kim Sangyeon <dho113@naver.com>
2019-05-26 12:23:31 +0900

    :rabbit: task/icon toggle
    - add icon toggle method

--- src/ts/Handler/MouseEventHandler.ts
+++ src/ts/Handler/MouseEventHandler.ts
@@ -23,6 +23,8 @@ class MouseEventHandler {
             e.stopPropagation();
             ACTION[actionName].apply(this);
         }
+
+        this.__toggleIcon__(e);
     }
 
     /**
@@ -32,6 +34,25 @@ class MouseEventHandler {
      */
     public onMouseUp = (e: any) => {
         this.selectionHanlder.saveSelectionRange();
+    }
+
+    /**
+     * Toggle Icon background
+     * @param {any} e
+     */
+    private __toggleIcon__(e: any) {
+        const elTarget = e.currentTarget;
+        const classList = elTarget.className.split(" ")
+        if (classList.indexOf("on") > -1) {
+            elTarget.className = classList.reduce((result: string, cl: string) => {
+                if (cl === "on") {
+                    return result;
+                }
+                return result = result.length > 0 ? result += " " + cl : result = cl;
+            }, "");
+        } else {
+            elTarget.className += " on";
+        }
     }
 }
 
