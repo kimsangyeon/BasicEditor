0c5170f8647bf11c8c2f9028982fc4f6255d98ee
Kim Sangyeon <dho113@naver.com>
2019-05-28 09:05:17 +0900

    :steam_locomotive: import Redo in Toolbar

--- src/Components/Toolbar/Toolbar.tsx
+++ src/Components/Toolbar/Toolbar.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import Bold from './Icons/Bold';
 import Italic from './Icons/Italic';
+import Redo from './Icons/Redo';
 import StrikeThrough from './Icons/StrikeThrough';
 import Underline from './Icons/Underline';
 import Undo from './Icons/Undo';
@@ -9,7 +10,7 @@ class Toolbar extends React.Component {
     public render() {
         return (
             <div id="be-toolbar" className="Toolbar">
-                <Bold /><Italic/><Underline/><StrikeThrough/><Undo/>
+                <Bold /><Italic/><Underline/><StrikeThrough/><Undo/><Redo/>
             </div>
         );
     }
