fe2e0583ccf008d0b642e704c8c48f8f9a72523c
Kim Sangyeon <dho113@naver.com>
2019-06-05 13:59:07 +0900

    :gem: Change the bold container class according to the bold state.

--- src/Components/Toolbar/Toolbar.tsx
+++ src/Components/Toolbar/Toolbar.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import ToolbarStatus from '../../ts/Toolbar';
 import Bold from './Icons/Bold';
 import Italic from './Icons/Italic';
 import Redo from './Icons/Redo';
@@ -6,11 +7,32 @@ import StrikeThrough from './Icons/StrikeThrough';
 import Underline from './Icons/Underline';
 import Undo from './Icons/Undo';
 
+interface IState {
+    bold: boolean
+}
+
 class Toolbar extends React.Component {
+    public state: IState = {
+        bold: false
+    }
+
+    private status: ToolbarStatus;
+
+    constructor(props: Readonly<{}>) {
+        super(props);
+        this.status = new ToolbarStatus(this.onSelectionChange);
+    }
+
+    public onSelectionChange = () => {
+        this.setState({
+            bold: this.status.getBold()
+        }); 
+    };
+
     public render() {
         return (
             <div id="be-toolbar" className="Toolbar">
-                <Bold /><Italic/><Underline/><StrikeThrough/><Undo/><Redo/>
+                <Bold bold={this.state.bold}/><Italic/><Underline/><StrikeThrough/><Undo/><Redo/>
             </div>
         );
     }
