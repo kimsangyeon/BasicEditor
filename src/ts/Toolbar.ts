b0e23c3fdcc13c2ad7ccc0569388613f50d37c7c
Kim Sangyeon <dho113@naver.com>
2019-06-05 13:57:51 +0900

    :diamonds: Add getBold method & update selectionchange bold status

--- src/ts/Toolbar.ts
+++ src/ts/Toolbar.ts
@@ -4,22 +4,36 @@ class Toolbar {
     protected strikethrough: boolean;
     protected underline: boolean;
     
-    constructor() {
+    constructor(fn: () => void) {
        this.bold = false;
        this.italic = false;
        this.strikethrough = false;
        this.underline = false;
 
-       this.__init__();
+       this.__init__(fn);
+    }
+
+    /**
+     * Return bold status
+     */
+    public getBold(): boolean {
+        return this.bold;
     }
     
     /**
      * Toolbar init method
      * add Event selection change
      */
-    private __init__(): void {
+    private __init__(fn: () => void): void {
         document.addEventListener('selectionchange', (e) => {
+            const sel: any = window.getSelection();
+            const focusNode: Node = sel.focusNode;
             
+            if (focusNode.parentElement) {
+                this.bold = !!focusNode.parentElement.closest('#basicEditor > b');
+            }        
+            
+            fn();
         });
     }
 }
