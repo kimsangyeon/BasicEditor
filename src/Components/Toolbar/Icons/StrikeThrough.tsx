53e57aa521c23764f4538664c51897faaa1a6bec
Kim Sangyeon <dho113@naver.com>
2019-07-16 23:08:11 +0900

    :necktie: modify strikethrough svg path

--- src/Components/Toolbar/Icons/StrikeThrough.tsx
+++ src/Components/Toolbar/Icons/StrikeThrough.tsx
@@ -24,11 +24,10 @@ class StrikeThrough extends React.Component {
             <div className={className} data-name={ACTION_NAME.STRIKE_THROUGH} onMouseDown={mouseEventHandler.onMouseDownIcon}>
                 <div className="be-icon">
                     <svg version="1.0" xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" viewBox="0 0 80.000000 80.000000" preserveAspectRatio="xMidYMid meet">
-                        <g transform="translate(0.000000,80.000000) scale(0.100000,-0.100000)" fill="#000000" stroke="none">
-                            <path d="M170 630 l0 -50 85 0 85 0 0 -85 0 -85 -75 0 c-43 0 -75 -4 -75 -10
-                                0 -6 77 -10 210 -10 133 0 210 4 210 10 0 6 -32 10 -75 10 l-75 0 0 85 0 85
-                                85 0 85 0 0 50 0 50 -230 0 -230 0 0 -50z m440 0 l0 -30 -85 0 -85 0 0 -95 0
-                                -95 -40 0 -40 0 0 95 0 95 -85 0 -85 0 0 30 0 30 210 0 210 0 0 -30z"/>
+                        <g transform="translate(10.000000,10.000000) scale(0.120000,0.120000)" fill="#000000" stroke="none">
+                            <path d="M420,85V0H25v85h155v115H35v55h145v190h85V255h145v-55H265V85H420z 	
+                                M250,430h-55V255h55V430z M395,215v25H50v-25H395z
+                                M250,70v130h-55V70H40V15h365v55H250z"/>
                         </g>
                     </svg>
                 </div>
